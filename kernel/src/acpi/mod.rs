6461cf3aac0d76d778ffbca634cc1351b3e128d7
Znmin <soliman.ramez@proton.me>
2025-05-19 22:36:19 +0200

    added params to acpihandler

--- kernel/src/acpi/mod.rs
+++ kernel/src/acpi/mod.rs
@@ -16,21 +16,56 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
+ // Currently only targeting x86
 use acpi::{AcpiHandler, PhysicalMapping};
+use core::ptr::NonNull;
 
- pub struct ACPIHandler;
+use crate::memory::alloc::HEAP_SIZE;
 
-impl AcpiHandler for ACPIHandler {
-    
-    unsafe fn map_physical_region<T>(&self, physical_address: usize, size: usize) -> acpi::PhysicalMapping<Self, T> {
+#[derive(Copy, Clone)]
+pub struct KernelAcpiHandler {}
+
+
+impl AcpiHandler for KernelAcpiHandler {
+    unsafe fn map_physical_region<T>(
+        &self,
+        physical_address: usize,
+        size: usize,
+    ) -> PhysicalMapping<Self, T> {
+
+        let virt_addr = crate::memory::phys_mem_offset() + physical_address as u64;
+
         unsafe {
             PhysicalMapping::new(
-            physical_start, 
-            virtual_start, 
-            region_length, 
-            mapped_length, 
-            ACPIHandler)
+            physical_address,
+            NonNull::new(virt_addr.as_mut_ptr()).unwrap(), // SAFETY: Memory at virt_addr is mapped by the HHDM and guaranteed to be valid by firmware
+            size,
+            HEAP_SIZE,
+            self.clone(),
+        )
         }
+      
     }
-    
+
+    fn unmap_physical_region<T>(_region: &PhysicalMapping<Self, T>) {
+        // only called to unload ACPI
+    }
+}
+
+#[test_case]
+fn acpi_map_physical_region_size_limit() {
+    let handler = KernelAcpiHandler {};
+
+    // Simulate a physical address (arbitrary for test)
+    let phys_addr = 0x1000usize;
+
+    // Test with size within HEAP_SIZE - should succeed
+    let size_within = crate::memory::alloc::HEAP_SIZE;
+    let mapping = unsafe { handler.map_physical_region::<u8>(phys_addr, size_within) };
+    assert_eq!(mapping.region_length(), size_within);
+
+    // Test with size larger than HEAP_SIZE - should panic
+    let size_too_large = crate::memory::alloc::HEAP_SIZE + 1;
+
+    // TODO: Continue writing test case
 }
